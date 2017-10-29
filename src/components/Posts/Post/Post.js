38f1fc2e4bb4cd0c2a5202ceb91c14e27022f929
Unknown <ale.steen@hotmail.com>
2017-10-29 17:17:54 +0100

    Can sudo upvote post

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './style.css';
 
-const Post = ({title, author, points, number}) => {
-    return (
-        <tr>
-            <td><span>{number}.</span> {title} <span className="host-url">(medium.com)</span><br/>
-            <span className="by-line">{points} points by {author}</span></td>
-        </tr>
-    );
+class Post extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { upvoted: false };
+
+        this.upvote = this.upvote.bind(this);
+    }
+
+    upvote() {
+        this.setState({ upvoted: !this.state.upvoted });
+    }
+
+    render() {
+        const { title, author, points, number } = this.props;
+
+        var upvoteClass = "glyphicon glyphicon-arrow-up";
+        upvoteClass += this.state.upvoted ? " upvoted" : "";
+        
+
+        return (
+            <tr>
+                <td><span>{number}. <i onClick={this.upvote} className={upvoteClass} /></span> {title} <span className="host-url">(medium.com)</span><br />
+                    <span className="by-line">{points} points by {author}</span></td>
+            </tr>
+        );
+    }
 }
 
 Post.propTypes = {
-    title: PropTypes.string.isRequired, 
+    title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
     points: PropTypes.number.isRequired,
     number: PropTypes.number.isRequired,
