de162aa093392b69e6666c6faa2971c0e14a1510
Unknown <ale.steen@hotmail.com>
2017-10-29 17:24:36 +0100

    Can downvote, but not both down and up vote

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -8,13 +8,28 @@ class Post extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { upvoted: false };
+        this.state = { upvoted: false, downvoted: false };
 
         this.upvote = this.upvote.bind(this);
+        this.downvote = this.downvote.bind(this);
     }
 
     upvote() {
+        if(this.state.downvoted){
+            alert("Can't both up and downvote");
+            return;
+        }
+
         this.setState({ upvoted: !this.state.upvoted });
+    }
+
+    downvote() {
+        if(this.state.upvoted){
+            alert("Can't both up and downvote");
+            return;
+        }
+
+        this.setState({ downvoted: !this.state.downvoted });
     }
 
     render() {
@@ -22,12 +37,21 @@ class Post extends Component {
 
         var upvoteClass = "glyphicon glyphicon-arrow-up";
         upvoteClass += this.state.upvoted ? " upvoted" : "";
-        
+
+        var downvoteClass = "glyphicon glyphicon-arrow-down";
+        downvoteClass += this.state.downvoted ? " downvoted" : "";
 
         return (
             <tr>
-                <td><span>{number}. <i onClick={this.upvote} className={upvoteClass} /></span> {title} <span className="host-url">(medium.com)</span><br />
-                    <span className="by-line">{points} points by {author}</span></td>
+                <td>
+                    <div>
+                        <span>{number}. <i onClick={this.upvote} className={upvoteClass} /><i onClick={this.downvote} className={downvoteClass} /></span>
+                    </div>
+                    <div>
+                        {title} <span className="host-url">(medium.com)</span><br />
+                        <span className="by-line">{points} points by {author}</span>
+                    </div>
+                </td>
             </tr>
         );
     }
