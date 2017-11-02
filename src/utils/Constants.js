ff24251e86468d6972b3d545bff4be8e35ced2fe
Alexander Steen <ale.steen@hotmail.com>
2017-11-02 14:48:24 +0100

    Changed connection to live API

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,3 +1,3 @@
 //export const apiBaseURL = "https://c0f8fc02-663e-449d-9bcd-ab8a7efdd6c4.mock.pstmn.io/"; // MOCK
-export const apiBaseURL = "http://fddf99c5.ngrok.io/";
-//export const apiBaseURL = "http://165.227.154.170:8080/";
+//export const apiBaseURL = "http://fddf99c5.ngrok.io/";
+export const apiBaseURL = "http://165.227.154.170:8080/";
